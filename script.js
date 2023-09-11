1e8ce1989b35ecc2f15d233c51502e9e00770295
Ashish Dungran <adungran@gmail.com>
2023-09-12 01:35:16 +0530

    Add function to play one round

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+
+function round(playerSelection, computerSelection) {
+    playerSelection = `${playerSelection.slice(0, 1).toUpperCase()}${playerSelection.slice(1).toLowerCase()}`;
+
+    if (playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Paper" && computerSelection === "Scissors" ||
+        playerSelection === "Scissors" && computerSelection === "Rock") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return "Tie!";
+    }
+}
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(round(playerSelection, computerSelection));
