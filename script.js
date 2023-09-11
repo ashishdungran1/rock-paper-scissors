cd11c7985983584d528cf87fa78343fe294f5aa9
Ashish Dungran <adungran@gmail.com>
2023-09-12 01:30:52 +0530

    Add function to get computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+    }
+}
